4b8f1ceb4300e35b10435ee84c5d6e0851e7b347
ptikoo1 <ptikoog@gmail.com>
2025-03-17 12:13:20 +0000

    Create sql_test_2__insert.sql

---
+++ sql_test_2/sql_test_2__insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO books (Title, Author, ISBN, Genre, PublicationYear, Price, StockQuantity)
+VALUES ('The Hitchhiker\'s Guide to the Galaxy', 'Douglas Adams', '9780345391803', 'Science Fiction', 1979, 12.99, 5);
